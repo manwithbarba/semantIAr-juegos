a58e9bf10dfd0d7dfeccb918d5604d395ef1b493
julián Sánchez viamonte <jsanchezviamonte@gmail.com>
2026-09-23 11:29:13 -0300

    fix: remove global instructions header from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Instructions from "@/components/Instructions";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +29,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Instructions />
         {children}
       </body>
     </html>
